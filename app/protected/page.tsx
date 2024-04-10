54f3d5779b410abd7032a8589c86547c53ac0b3c
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-04-10 11:16:15 +0530

    changes

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -8,11 +8,15 @@ export default function ProtectedPage() {
   const supabase = createClient();
   const [uid, setUid] = useState(0);
   const [email, setEmail] = useState("");
+  const [colorMode, setColorMode] = useState("light");
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
+      console.log(session?.user?.email);
       if (!session?.user) {
         return redirect("/login");
+      } else {
+        setEmail(session?.user?.email ?? "");
       }
     });
   }, [supabase]);
@@ -29,11 +33,12 @@ export default function ProtectedPage() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Cubid Integration</h2>
-          <form
-            onSubmit={async (e) => {
+
+          <button
+            onClick={async (e) => {
               e.preventDefault();
               const { data } = await axios.post(
-                "https://passport.cubid.me/api/dapp/create_user",
+                "http://localhost:3000/api/dapp/create_user",
                 {
                   dapp_id: 34,
                   email,
@@ -42,45 +47,43 @@ export default function ProtectedPage() {
               const { uuid } = data;
               setUid(uuid);
             }}
-            className="text-center space-y-3 mt-4 rounded-lg bg-base-300 p-12"
+            type="submit"
+            className="btn btn-accent w-full"
           >
-            <label className="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="w-4 h-4 opacity-70"
-              >
-                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-              </svg>
-              <input
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                type="text"
-                required
-                className="w-full bg-transparent"
-                placeholder="Email"
-              />
+            Get Uuid
+          </button>
+          <form className="max-w-sm mx-auto">
+            <label
+              htmlFor="countries"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Choose Color Mode
             </label>
-            <button type="submit" className="btn btn-accent w-full">
-              Submit
-            </button>
-            {Boolean(uid) && (
-              <>
-                <a
-                  href={`https://passport.cubid.me/allow?uid=${uid}`}
-                  onClick={() => {
-                    localStorage.setItem("uid-to-fetch", uid as any);
-                  }}
-                  className="link link-accent"
-                >
-                  Redirect to uuid
-                </a>
-              </>
-            )}
+            <select
+              id="countries"
+              value={colorMode}
+              onChange={(e) => {
+                setColorMode(e.target.value);
+              }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option value="light">Light Mode</option>
+              <option value="dark">Dark Mode</option>
+            </select>
           </form>
+          {Boolean(uid) && (
+            <>
+              <a
+                href={`https://passport.cubid.me/allow?uid=${uid}&colormode=${colorMode}`}
+                onClick={() => {
+                  localStorage.setItem("uid-to-fetch", uid as any);
+                }}
+                className="link link-accent"
+              >
+                Redirect to uuid
+              </a>
+            </>
+          )}
         </main>
       </div>
 
