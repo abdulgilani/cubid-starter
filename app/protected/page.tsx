0dcd9a603770413abbf798a85c5affa6c251868b
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-04-13 12:30:43 +0800

    Changes

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -7,8 +7,47 @@ import { useEffect, useState } from "react";
 export default function ProtectedPage() {
   const supabase = createClient();
   const [uid, setUid] = useState(0);
+  const [userCreated, setUserCreated] = useState(false);
   const [email, setEmail] = useState("");
   const [colorMode, setColorMode] = useState("light");
+  const [apiData, setApiData] = useState({
+    score: "",
+    identity: "",
+    score_details: "",
+  });
+
+  const fetchDataSchema = {
+    get_score: async () => {
+      const { data } = await axios.post(
+        "http://localhost:3001/api/dapp/fetch_score",
+        {
+          apikey: "8c354e51-d323-482a-86ca-e931cd0e91d8",
+          uid: uid,
+        }
+      );
+      setApiData((d) => ({ ...d, score: data }));
+    },
+    get_identity: async () => {
+      const { data } = await axios.post(
+        "http://localhost:3001/api/dapp/get_identity",
+        {
+          apikey: "8c354e51-d323-482a-86ca-e931cd0e91d8",
+          uid: uid,
+        }
+      );
+      setApiData((d) => ({ ...d, identity: data }));
+    },
+    get_score_details: async () => {
+      const { data } = await axios.post(
+        "http://localhost:3001/api/dapp/get_score_details",
+        {
+          apikey: "8c354e51-d323-482a-86ca-e931cd0e91d8",
+          uid: uid,
+        }
+      );
+      setApiData((d) => ({ ...d, score_details: data }));
+    },
+  };
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
@@ -20,6 +59,8 @@ export default function ProtectedPage() {
       }
     });
   }, [supabase]);
+
+  const { score, identity, score_details } = apiData;
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -33,18 +74,19 @@ export default function ProtectedPage() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Cubid Integration</h2>
-
+          <p>{email}</p>
           <button
             onClick={async (e) => {
               e.preventDefault();
               const { data } = await axios.post(
-                "http://localhost:3000/api/dapp/create_user",
+                "https://passport.cubid.me/api/dapp/create_user",
                 {
                   dapp_id: 34,
                   email,
                 }
               );
-              const { uuid } = data;
+              const { uuid, newuser } = data;
+              setUserCreated(newuser);
               setUid(uuid);
             }}
             type="submit"
@@ -52,6 +94,8 @@ export default function ProtectedPage() {
           >
             Get Uuid
           </button>
+          <p>Uuid created for user - {uid}</p>
+          <p>New User Created - {userCreated ? "TRUE" : "FALSE"}</p>
           <form className="max-w-sm mx-auto">
             <label
               htmlFor="countries"
@@ -82,6 +126,59 @@ export default function ProtectedPage() {
               >
                 Redirect to uuid
               </a>
+              <p>
+                Argument Sent to API's{" "}
+                {JSON.stringify({
+                  apikey: "8c354e51-d323-482a-86ca-e931cd0e91d8",
+                  uid: uid,
+                })}
+              </p>
+              <div className="space-y-2">
+                <div className="grid grid-cols-5">
+                  <div>/fetch_score</div>
+                  <div className="mx-2">
+                    <button
+                      className="bg-blue-500 text-xs w-full py-2 rounded-md"
+                      onClick={fetchDataSchema.get_score}
+                    >
+                      Fetch Score
+                    </button>
+                  </div>
+                  <div className="col-span-3">
+                    {Boolean(apiData.score) && JSON.stringify(apiData.score)}
+                  </div>
+                </div>
+                <div className="grid grid-cols-5">
+                  <div>/get_identity</div>
+                  <div className="mx-2">
+                    <button
+                      className="bg-blue-500 text-xs w-full py-2 rounded-md"
+                      onClick={fetchDataSchema.get_identity}
+                    >
+                      Get Identity
+                    </button>
+                  </div>
+                  <div className="col-span-3">
+                    {Boolean(apiData.identity) &&
+                      JSON.stringify(apiData.identity)}
+                  </div>
+                </div>
+                <div className="grid grid-cols-5">
+                  <div>/get_score_details</div>
+                  <div className="mx-2">
+                    <button
+                      className="bg-blue-500 text-xs w-full py-2 rounded-md"
+                      onClick={fetchDataSchema.get_score_details}
+                    >
+                      Get Score Details
+                    </button>
+                  </div>
+                  <div className="col-span-3">
+                    {Boolean(apiData.score_details) &&
+                      JSON.stringify(apiData.score_details)}
+                  </div>
+                </div>
+              </div>
             </>
           )}
         </main>
