d8bd9c0ffc69827a1b3a584553f499cbb21bb4d2
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-08-09 01:45:00 +0530

    changes

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -97,6 +97,7 @@ export default function ProtectedPage() {
             onClick={async (e) => {
               e.preventDefault();
              await supabase.auth.signOut();
+             window.location.href = window.location.origin;
             }}
             type="submit"
             className="btn btn-accent w-full"
@@ -105,25 +106,6 @@ export default function ProtectedPage() {
           </button>
           <p>Uuid created for user - {uid}</p>
           <p>New User Created - {userCreated ? "TRUE" : "FALSE"}</p>
-          <form className="max-w-sm mx-auto">
-            <label
-              htmlFor="countries"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Choose Color Mode
-            </label>
-            <select
-              id="countries"
-              value={colorMode}
-              onChange={(e) => {
-                setColorMode(e.target.value);
-              }}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            >
-              <option value="light">Light Mode</option>
-              <option value="dark">Dark Mode</option>
-            </select>
-          </form>
           {Boolean(uid) && (
             <>
               <a
